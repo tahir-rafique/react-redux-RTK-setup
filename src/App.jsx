40bb4756ce7c1a425bad5bc7eef8b4430a8f3403
itstahirrafique <121024103+tahir-rafique@users.noreply.github.com>
2025-10-21 12:14:03 +0500

    some ui changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react"
-import { Counter } from "./Counter"
-// import { Counter } from "./store/features/counter/Counter"
+import { Counter } from './Components/Counter';
 
 function App() {
 
   return (
-    <div>
+    <div className=" bg-[#2f4fa6] h-screen w-screen flex justify-center items-center relative ">
+      <h1 className="absolute top-7 left-1/2 -translate-x-1/2 text-lg sm:text-xl lg:text-3xl font-semibold underline  decoration-white decoration-wavy text-white underline-offset-4 text-center ">React Redux-RTK(redux toolkit) Counter App</h1>
       <Counter />
+      <h1 className="absolute bottom-10 left-1/2 -translate-x-1/2 text-base sm:text-lg lgtext-xl font-semibold underline  decoration-white decoration-wavy text-white underline-offset-4 font-mono text-center  ">Created with &#9829; (love) by Tahir!</h1>
     </div>
   )
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './store/features/counter/counterSlice'
+import { decrement, increment, incrementByAmount } from '../store/features/counter/counterSlice'
 
 export function Counter() {
     const count = useSelector((state) => state.counter.value)
     const dispatch = useDispatch()
 
     return (
-        <div>
+        <div className=' w-full max-w-[500px] flex flex-col justify-center items-center'>
             <button
+                className=' bg-pink-300 p-2 rounded-md shadow-md font-mono font-bold cursor-pointer'
                 aria-label="Increment value"
                 onClick={() => dispatch(increment())}
             >
                 Increment
             </button>
-            <span>{count}</span>
+            <span className='h-[100px] w-[100px] text-center flex justify-center items-center bg-white my-4 rounded-md drop-shadow-2xl shadow-md font-mono text-4xl'>{count}</span>
             <button
+                className=' bg-pink-300 p-2 rounded-md shadow-md font-mono font-bold cursor-pointer'
                 aria-label="Decrement value"
                 onClick={() => dispatch(decrement())}
             >
                 Decrement
             </button>
-            <div style={{
-                marginTop: '20px'
-            }}>
+
+            <div
+                className='bg-white p-2 rounded-md shadow-md font-mono text-lg font-semibold cursor-pointer'
+                style={{
+                    marginTop: '20px'
+                }}
+            >
                 <button
+                    className='bg-pink-300 p-3 rounded-md shadow-md font-mono text-lg font-semibold cursor-pointer'
                     aria-label="Decrement value"
                     onClick={() => {
                         if (confirm("Are you sure you want to increase by 10?")) {
